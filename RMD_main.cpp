9a135a1fd95df3a04221054ea16a45fedf8d3211
Kyungjin Bae <leonardobae@naver.com>
2018-01-03 12:29:36 +0900

    Main

--- RMD_main.cpp
+++ RMD_main.cpp
@@ -14,45 +14,76 @@ int main()
 {
 	RMD RMD;
 	string vid_name;
+	bool stop = false;
+	char key;	
+
+	/*---------Variables for histogram---------*/
+	Mat histogram;
+	float channel_range[] = { 0, 256 };
+	const float* channel_ranges = { channel_range };
+	int number_bins = 256;
+	bool uniform = true;
+	bool accumulate = false;
+	/*----------------------------------------*/
+
 
 	cout << "Enter video name: ";
 	getline(cin, vid_name);
-
 	VideoCapture vid(vid_name);
 
-	if (!vid.isOpened()) {
-		cerr << "Video File Open Error" << endl;
-		exit(1);
-	}
-
-	/*동영상 정보 출력*/
+	/*-----Variables for video information-----*/
 	float _fps = vid.get(CV_CAP_PROP_FPS);
 	int _cols = vid.get(CV_CAP_PROP_FRAME_WIDTH);
 	int _rows = vid.get(CV_CAP_PROP_FRAME_HEIGHT);
 	double _nframe = vid.get(CV_CAP_PROP_FRAME_COUNT);
 	Size _ImageSize = Size(_cols, _rows);
+	/*-----------------------------------------*/
 
+		
+	if (!vid.isOpened()) {
+		cerr << "Video File Open Error" << endl;
+		exit(1);
+	}
+	
+	/*-------------Output video information-------------------*/
 	cout << endl;
 	cout << "Input Video File Name : " << "test2" << endl;
 	cout << "Frame Per Seconds : " << _fps << endl;
 	cout << "Frame Size : " << _cols << " x " << _rows << endl;
 	cout << "Frame Count : " << _nframe << endl;
+	/*--------------------------------------------------------*/
 
-	/*각 프레임에 ROI 씌우기*/
-	Mat frame;
-	Mat warpImg;
-	bool stop = false;
-	char key;
 
 	while (1)
 	{
 		if (!stop)
 		{
-			if (!vid.read(frame))
+			if (!vid.read(RMD.cur_frame))
 				break;
 			fr_cnt = vid.get(CV_CAP_PROP_POS_FRAMES);
+			
+			imshow("Original", RMD.cur_frame);												//display current frame
 
-			imshow("Original", frame);
+			/* --<Warping Rect Information>-- */
+			//1. good4.mpg = 
+
+			/*--------------------------------*/
+
+			RMD.warp_img = RMD.WarpImage(RMD.cur_frame(Rect(480, 450, 380, 200)));
+			imshow("Warp", RMD.warp_img);
+
+			cvtColor(RMD.warp_img, RMD.gray_frame, CV_BGR2GRAY);
+
+			calcHist(&RMD.gray_frame, 1, 0, Mat(), histogram, 1, &number_bins, &channel_ranges, uniform, accumulate);
+
+			vector<int> peaks = RMD.getLocalMaximum(histogram, 9, 3, 0.5);
+
+			if (RMD.flag)
+			{
+				threshold(RMD.gray_frame, RMD.binary_frame, RMD.thresh_val, 255, CV_THRESH_BINARY);
+				imshow("Threshold", RMD.binary_frame);
+				RMD.Cont(RMD.binary_frame);
+			}
 
 		}
 
@@ -60,22 +91,25 @@ int main()
 
 		if (key == 27)
 			break;
-		else if (key == 32) {
+		else if (key == 32)
+		{
 			if (stop == true)
-
 				stop = false;
 			else if (stop == false)
 				stop = true;
 		}
-		else if (key == ']') {
+		else if (key == ']')
+		{
 			fr_cnt += 100;
 			vid.set(CV_CAP_PROP_POS_FRAMES, fr_cnt - 1);
 		}
-		else if (key == '[') {
+		else if (key == '[')
+		{
 			fr_cnt -= 100;
 			vid.set(CV_CAP_PROP_POS_FRAMES, fr_cnt - 1);
 		}
-		else if (key == '.') {
+		else if (key == '.')
+		{
 			fr_cnt += 10;
 			vid.set(CV_CAP_PROP_POS_FRAMES, fr_cnt - 1);
 		}
